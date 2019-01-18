9d46bae310bb00d1cc7fe0b197224e4f60413994
jianlong90 <46743361+jianlong90@users.noreply.github.com>
2019-01-18 17:39:14 +0800

    Update Launcher.java

--- src/android/Launcher.java
+++ src/android/Launcher.java
@@ -532,9 +532,9 @@ public boolean execute(String action, JSONArray args, CallbackContext callbackCo
 	}*/
 	public void onActivityResult(int requestCode, int resultCode, Intent data) 
 {
-	    PluginResult result = new PluginResult(PluginResult.Status.OK,requestCode);
-            result.setKeepCallback(true);
-            callback.sendPluginResult(result);
+	    PluginResult result2 = new PluginResult(PluginResult.Status.OK,requestCode);
+            result2.setKeepCallback(true);
+            callback.sendPluginResult(result2);
     if( requestCode == LAUNCH_REQUEST )
     {
         if( resultCode == Activity.RESULT_OK )
